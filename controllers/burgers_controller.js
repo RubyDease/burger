4f03ff33b1b0cf5272d2b8243eca8d99695998ea
Ruby Dease <rubydease@me.com>
2017-09-03 17:17:21 -0400

    Update burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,38 +1,45 @@
 var express = require('express');
+
 var router = express.Router();
-var bodyParser = require('body-parser');
+
 var burger = require('../models/burger.js');
 
-router.use(bodyParser.urlencoded({
-  extended: false
-}));
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-router.get('/', function(req,res) {
-  burger.all(function(data){
-    res.render('index', {data : data});
-  });
+// add a '/' endpoint that redirects to the /index route
+router.get('/', function(req, res) {
+	res.redirect('/index');
 });
 
-router.post('/delete/:burger_name', function(req,res) {
-  var burger_name = req.params.burger_name;
-  burger.delete(burger_name, function(data){
-    res.redirect('/');
-  });
+// add a '/index/' endpoint that gets all the burgers
+// then renders the index file by passing in all the burgers
+// as an object for handlebars to use
+router.get('/index', function(req, res) {
+	burger.all(function(data) {
+		var hbsObject = {burger: data};
+		console.log(hbsObject);
+		res.render('index', hbsObject);
+	});
 });
 
-router.post('/eat/:burger_name', function(req,res) {
-  var burger_name = req.params.burger_name;
-  burger.eat(burger_name, function(data){
-    res.redirect('/');
-  });
+// add a '/burgers/create' endpoint that posts the 
+// burger name the user entered then as a callback it
+// redirects back to the /index route
+router.post('/burger/create', function(req, res) {
+	burger.create(['burger_name', 'devoured'], [req.body.name, false], function() {
+		res.redirect('/index');
+	});
 });
 
-router.post('/add/', urlencodedParser, function(req,res) {
-  var burger_name = req.body.burgertoadd;
-  burger.add(burger_name, function(data){
-    res.redirect('/');
-  });
+// add a '/burgers/update/:id' route that updates
+// the status of the burger from being uneaten to eaten
+// then does a callback that redirects to the /index endpoint
+router.put('/burger/update/:id', function(req, res) {
+	var condition = 'id = ' + req.params.id;
+	console.log('condition', condition);
+
+	burger.update({devoured: req.body.devoured}, condition, function() {
+		res.redirect('/index');
+	});
 });
 
+// export the router (controller) back to the server
 module.exports = router;
