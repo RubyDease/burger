08a59a538f7209308694d47116d7ef026878c2a7
Ruby Dease <rubydease@me.com>
2017-09-03 17:25:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require('method-override')
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// set port to 3000 
+var PORT = process.env.PORT || 3000;
+
+// Create an instance of the express app.
 var app = express();
 
-var port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+/// bodyparsers 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// override with POST having ?_method=DELETE or PUT
+app.use(methodOverride('_method'));
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
-
-var exphbs = require("express-handlebars");
-
+// Set Handlebars as the default templating engine.
+var exphbs = require('express-handlebars');
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var route = require("./controllers/burgers_controller.js");
-app.use("/", route);
+// now import the routes
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
 
-
-app.listen(port);
+// Initiate the listener.
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
