589a0fa53e9a1e98d69ea41c13dc53398c9e6319
Ruby Dease <rubydease@me.com>
2017-09-03 17:16:20 -0400

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,6 @@
-// Import MySQL connection.
-var connection = require("../config/connection.js");
+var connection = require('../config/connection.js');
 
-// Helper function for SQL syntax.
+
 function printQuestionMarks(num) {
     var arr = [];
 
@@ -11,71 +10,77 @@ function printQuestionMarks(num) {
 
     return arr.toString();
 }
-
-// Helper function for SQL syntax.
+// helper function fdor sql syntax.
 function objToSql(ob) {
     var arr = [];
 
     for (var key in ob) {
         if (Object.hasOwnProperty.call(ob, key)) {
-            arr.push(key + "=" + ob[key]);
+			arr.push(key + '=' + ob[key]);
         }
     }
 
     return arr.toString();
+
 }
 
-// Object for all our SQL statement functions.
+
+// Object for all our SQL statement functions
 var orm = {
-    all: function(tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-
-        connection.query(queryString, function(err, result) {
-            if (err)
-                throw err;
-            cb(result);
-        });
-    },
-
+        all: function(tableInput, cb) {
+            var queryString = "SELECT * FROM " + tableInput + ";";
+            connection.query(queryString, function(err, result) {
+                    if (err)throw err;
+                    // send query result back to the callback function
+                    cb(result);
+                });
+            },
+  
+    //vals is an array of values that we want to save to cols
+    //cols are the columns we want to insert the values into
+    // cb is callback
     create: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
+        var queryString = 'INSERT INTO ' + table;
 
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
+        queryString =+ ' (';
+        queryString =+ cols.toString();
+        queryString =+ ') ';
+        queryString =+ 'VALUES (';
+        queryString =+ printQuestionMarks(vals.length);
+        queryString =+ ') ';
 
         console.log(queryString);
+        console.log(vals);
 
         connection.query(queryString, vals, function(err, result) {
-            if (err) {
-                throw err;
+            if (err) throw err;
+            // send query result back to the callback for function
                 cb(result);
-            }
-
+            
         });
     },
+    // update one function for changing the burger status
 
     update: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
+        var queryString = 'UPDATE ' + table;
 
-        queryString += " SET ";
+        queryString += ' SET ';
         queryString += objToSql(objColVals);
-        queryString += " WHERE ";
+        queryString += ' WHERE ';
         queryString += condition;
 
         console.log(queryString);
+
         connection.query(queryString, function(err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
+            if (err) throw err;
+            // send the query result back to the callback function
+                cb(result);
+            
+
         });
     },
     
 };
-
 // Export the orm object for the model (burger.js).
+
 module.exports = orm;
